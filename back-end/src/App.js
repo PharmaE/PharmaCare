73b9e8bcb5d51acd067e4a2832a354b92c9e304a
JaspreetKaur <JK359@myscc.ca>
2022-12-14 13:14:28 -0500

    del nav

--- back-end/src/App.js
+++ back-end/src/App.js
@@ -36,43 +36,7 @@ function App() {
   }, []);
 
   return (
-    <>
-      <Space direction="vertical" style={{ display: "flex" }}>
-        <Row className="nav">
-          <h1 className="h1">PharmaCare</h1>
-          <nav className="link">
-            <Link to="/home" className="li">
-              Home
-            </Link>
-            <Link to="/shop" className="li">
-              Shop
-            </Link>
-            <Link to="/doc" className="li">
-              Docs
-            </Link>
-            <Link to="/contactus" className="li">
-              Contact Us
-            </Link>
-            <Link to="/" className="lilog">
-              Log Out
-            </Link>
-          </nav>
-        </Row>
-      </Space>
-      <div>
-        <Routes>
-          <Route path="/" element={<Login auth={authenticate} />} />
-          <Route
-            path="/home"
-            element={<Home logout={logout} status={status} />}
-          />
-          <Route path="/shop" element={<Medication />} />
-          <Route path="/doc" element={<Doc />} />
-        </Routes>
-      </div>
-
-      <Footer />
-    </>
+ <></>
   );
 }
 
