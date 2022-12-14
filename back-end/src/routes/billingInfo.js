9bdb9d40115b31dd250242e47f6707a7654a97a3
Ajima Master <98704309+masterajima@users.noreply.github.com>
2022-12-14 13:09:06 -0500

    delete folder

--- back-end/src/routes/billingInfo.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const router = express.Router();
-
-import { addBillingInfo } from "../controllers/billingInfo.js";
-
-router.post("/addBillingInfo", addBillingInfo);
-
-export default router;
--- back-end/src/routes/medicalInfo.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { addMedicalInfo } from "../controllers/medicalInfo.js";
-const router = express.Router();
-
-
-router.post("/addMedicalInfo", addMedicalInfo);
-
-export default router;
